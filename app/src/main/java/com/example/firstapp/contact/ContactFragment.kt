db1f71b209e9dded0656a9c3fff745ef1af89f6f
nichanun.pl <nichanun.pl@rmuti.ac.th>
2020-08-28 01:41:16 +0700

    Update ContactFragment

--- app/src/main/java/com/example/firstapp/contact/ContactFragment.kt
+++ app/src/main/java/com/example/firstapp/contact/ContactFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.firstapp.R
+import com.example.firstapp.database.ContactDatabase
 import com.example.firstapp.databinding.FragmentContactBinding
 
 
@@ -22,7 +24,18 @@ class ContactFragment : Fragment() {
             container,
             false
         )
+
         setHasOptionsMenu(true)
+
+        val application = requireNotNull(this.activity).application
+        val dataSource = ContactDatabase.getInstance(application).databaseDao
+        val viewModelFactory = ContactViewModelFactory(dataSource, binding, application)
+        val contactViewModel =
+            ViewModelProvider(
+                this, viewModelFactory
+            ).get(ContactViewModel::class.java)
+        binding.contactViewModel = contactViewModel
+        binding.lifecycleOwner = this
         return binding.root
     }
 
