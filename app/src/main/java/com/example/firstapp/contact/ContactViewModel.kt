96dfdaa161d078c973fc85e1d4b231dae3853cec
nichanun.pl <nichanun.pl@rmuti.ac.th>
2020-08-28 01:35:13 +0700

    Update ContactViewModel,ContactViewModelFactory

--- app/src/main/java/com/example/firstapp/contact/ContactViewModel.kt
+++ app/src/main/java/com/example/firstapp/contact/ContactViewModel.kt
@@ -1,4 +1,13 @@
 package com.example.firstapp.contact
 
-class ContactViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.firstapp.database.DatabaseDAO
+import com.example.firstapp.databinding.FragmentContactBinding
+
+class ContactViewModel(
+    val database: DatabaseDAO,
+    private val binding: FragmentContactBinding,
+    application: Application
+) : AndroidViewModel(application) {
 }
--- app/src/main/java/com/example/firstapp/contact/ContactViewModelFactory.kt
+++ app/src/main/java/com/example/firstapp/contact/ContactViewModelFactory.kt
@@ -1,4 +1,20 @@
 package com.example.firstapp.contact
 
-class ContactViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.firstapp.database.DatabaseDAO
+import com.example.firstapp.databinding.FragmentContactBinding
+
+class ContactViewModelFactory (
+    private val dataSource: DatabaseDAO,
+    private val binding: FragmentContactBinding,
+    private val application: Application
+) : ViewModelProvider.Factory{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ContactViewModel::class.java)) {
+            return ContactViewModel(dataSource, binding, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
